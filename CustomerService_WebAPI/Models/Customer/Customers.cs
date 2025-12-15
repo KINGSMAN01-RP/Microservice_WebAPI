df41a6169b2b7cf6f1769c79bf645add42a11c1e
KINGSMAN01-RP <Kingsmanp40@gmail.com>
2025-12-16 00:23:18 +0530

    Adding new model

--- CustomerService_WebAPI/Models/Customer/Customers.cs
+++ CustomerService_WebAPI/Models/Customer/Customers.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
         public DateTime DateCreated { get; set; }
         public DateTime? DateModified { get; set; }
     }
